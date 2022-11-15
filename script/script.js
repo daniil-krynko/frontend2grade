3400fbd8f72d10d2c691db05a1545d561a9b388f
Daniil Krynko <daniil.krynko@gmail.com>
2022-11-15 13:22:20 +0200

    All done

--- script/script.js
+++ script/script.js
@@ -1,20 +1,67 @@
-let words = {
-    nag:"шкапа",
-    elbow: "лікоть",
-    magician:"волхв",
-    acorn:"жолуд",
-    coccyx:"куприк",
-    hammock:"гамак",
-    perch:"жердинка",
-    cast:"зліпок",
-    australopithecine:"австралопитек",
-    wineglass:"келих"
-};
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
+$("#Modal").hide();
+$(".modalWindow button").click(restart);
+$("body>button").click(next);
+let eng = [
+  'nag',
+  'elbow',
+  'magician',
+  'acorn',
+  'coccyx',
+  'hammock',
+  'perch',
+  'cast',
+  'australopithecine',
+  'wineglass'
+];
+let ukr = [
+  "шкапа",
+  "лікоть",
+  "волхв",
+  "жолуд",
+  "куприк",
+  "гамак",
+  "жердинка",
+  "зліпок",
+  "австралопитек",
+  "келих"
+];
+let scTrue = 0, scFalse = 0, counter = 0;
+function shuffle(eng, ukr) {
+  for (let i = eng.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [eng[i], eng[j]] = [eng[j], eng[i]];
+    [ukr[i], ukr[j]] = [ukr[j], ukr[i]];
   }
 }
-shuffle(words);
-document.getElementById("Card").textContent = words[0];
+function next() {
+  if ($('input')[0].value.toLowerCase() == ukr[counter].toLowerCase()) {
+    $('#True').text(++scTrue);
+  } else {
+    $('#False').text(++scFalse);
+  }
+  if (counter == 9) {
+    $("#Modal").show();
+    if(scTrue == 10) {
+      $("#Message").text("Вітаємо! Ви вгадали всі слова")
+      $("#Message").css("color", "green");
+    } else {
+      $("#Message").text(`Ви вгадали ${scTrue} з ${eng.length}`);
+      $("#Message").css("color", "red");
+    }
+    return;
+  }
+  $("#Card").text(eng[++counter]);
+  $("#Counter").text(`${counter+1}/10`);
+  $('input')[0].value = null;
+}
+function restart() {
+  shuffle(eng, ukr);
+  counter = scTrue = scFalse = 0;
+  $("#Counter").text(`${counter+1}/10`);
+  $("#True").text(scTrue);
+  $("#False").text(scFalse);
+  $("#Card").text(eng[counter]);
+  $("#Modal").hide();
+}
+shuffle(eng, ukr);
+$("#Card").text(eng[counter]);
