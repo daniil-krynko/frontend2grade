9037d630b372f90724bfc7ded62a490287061f62
Daniil Krynko <daniil.krynko@gmail.com>
2022-12-09 01:42:08 +0200

    All done

--- script/script.js
+++ script/script.js
@@ -1,9 +1,8 @@
 let userName = prompt("Enter your name");
-if (userName != null && userName != "") {
-   document.getElementById("UserName").textContent = userName;
-}
-let roundCnt = 0, maxRnd = 3;
-let slots = [3][3];
+if (userName != null && userName != "")
+   document.getElementById("UserName").innerHTML = userName;
+document.getElementById("Generate").addEventListener("click", Generate);
+let roundCnt = 1, maxRnd = 3;
 let symbols = [
     `<img src="images/apple.png">`,
     `<img src="images/cherry.png">`,
@@ -14,42 +13,59 @@ let symbols = [
     `<img src="images/strawberry.png">`,
     `<img src="images/watermelon.png">`
 ];
-function RowTest(el, index, arr) {
-    if (index === 0) {
-        return true;
-    } else {
-        return (el.answer === arr[index - 1].answer);
-    }
-}
-function ColumnTest() {
-    for (let j = 0; j < 3; j++) {
-        for (let i = 0; i < 3; i++) {
-            if (slots[i][j]) {
-                
-            }
+
+function Victory() {
+    for (let i = 0; i < 3; i++) {
+        if (RowTest(i)) {
+            return true;
         }
     }
+    return false;
+}
+
+function RowTest(currentRow) {
+    for (let j = 0; j < 2; j++)
+        if(document.getElementById(`${currentRow}${j}`).innerHTML !=
+        document.getElementById(`${currentRow}${j+1}`).innerHTML)
+            return false;
     return true;
 }
-function Randomizer() {
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
 
-        }
-        if(!ColumnTest()){
-            i--;
-        }
-    }
-    if (row[0].every(RowTest) || row[1].every(RowTest) || row[2].every(RowTest)) {
-        alert("YOU WON!!!");
-        roundCnt = 0, maxRnd = 3;
+function ColumnTest(currentColumn) {
+    if (document.getElementById(`${0}${currentColumn}`).innerHTML ==
+    document.getElementById(`${1}${currentColumn}`).innerHTML ==
+    document.getElementById(`${2}${currentColumn}`).innerHTML) {
+        return false;
+    } else {
+        return true;
     }
 }
+
+function Randomizer() {
+    for (let j = 0; j < 3; j++) {
+        if(j != 0 && !ColumnTest(j))
+            j--;
+        for (let i = 0; i < 3; i++) {
+            document.getElementById(`${i}${j}`).innerHTML =
+            symbols[Math.floor(Math.random() * symbols.length)];
+        }
+    }
+}
+
 function Generate() {
-    if (roundCnt == maxRnd) {
-        alert(`You lose. Better luck next time!`);
-        roundCnt = 0, maxRnd = 3;
+    if (Victory()) {
+        alert("YOU WON!!!");
+        roundCnt = 1, maxRnd = 3;
+        document.getElementById("RoundCnt").innerHTML = `Спроба ${roundCnt} з ${maxRnd}`;
+        Randomizer();
+        return;
+    } else if (roundCnt == maxRnd) {
+        alert("You lose. Better luck next time!");
+        roundCnt = 1, maxRnd = 3;
+        document.getElementById("RoundCnt").innerHTML = `Спроба ${roundCnt} з ${maxRnd}`;
+        Randomizer();
+        return;
     }
     Randomizer();
-    document.getElementById("RoundCnt").innerText = `Спроба ${++roundCnt} з ${maxRnd}`;
+    document.getElementById("RoundCnt").innerHTML = `Спроба ${++roundCnt} з ${maxRnd}`;
 }
