56f306fe9db8e2b4f704250674d9e5b76b7ea277
Daniil Krynko <daniil.krynko@gmail.com>
2022-12-15 16:11:47 +0200

    Reworked drawing a graph

--- script/script.js
+++ script/script.js
@@ -1,41 +1,55 @@
 function TabulateF() {
     // y = ln ( x^2 + e^|x| + 1)
     // m.log(m.pow(x, 2) + m.exp(m.abs(x)) + 1);
-
-    let h = 300, w = 500; // Height and width of canvas
+    let h = document.getElementById("MyCanvas").clientHeight,
+    w = document.getElementById("MyCanvas").clientWidth; // Height and width of canvas
     let from = Number(document.getElementById("IntervalFrom").value);
+    if (from == null || from == '') {
+        from = 1;
+    }
     let to = Number(document.getElementById("IntervalTo").value);
+    if (to == null || to == '') {
+        to = 10;
+    }
     let step = Number(document.getElementById("Step").value);
-    let m = Math;
-    const whileLoop = [];
-    const dowhileLoop = [];
-    const forLoop = [];
-
+    if (step == null || step == '') {
+        step = 0.5;
+    }
+    let m = Math, scX = w/10, scY = h/10;
     /* While loop */
     let x = from;
+    console.log("While loop");
     while (to >= x) {
-        whileLoop.push(m.log(m.pow(x, 2) + m.exp(m.abs(x)) + 1));
+        console.log(`${x} : ${m.log(m.pow(x, 2) + m.exp(m.abs(x)) + 1)}`);
         x += step;
     }
-    console.log(`While loop: ${whileLoop}`);
-
     /* DoWhile loop */
     x = from;
+    console.log("DoWhile loop");
     do {
-        dowhileLoop.push(m.log(m.pow(x, 2) + m.exp(m.abs(x)) + 1));
+        console.log(`${x} : ${m.log(m.pow(x, 2) + m.exp(m.abs(x)) + 1)}`);
         x += step;
     } while (to >= x);
-    console.log(`DoWhile loop: ${dowhileLoop}`);
-
     /* For loop */
-    x = from;
+    console.log("For loop");
+    for (x = from; to >= x; x += step) {
+        console.log(`${x} : ${m.log(m.pow(x, 2) + m.exp(m.abs(x)) + 1)}`);
+    }
+    // Axises
     let ctx = document.getElementById("MyCanvas").getContext("2d");
     ctx.beginPath();
-    ctx.moveTo(0, h/2);
-    for (i = 0; to >= x; x += step, i+= step){
-        forLoop.push(m.log(m.pow(x, 2) + m.exp(m.abs(x)) + 1));
-        ctx.lineTo(i, h/2 - forLoop[forLoop.length - 1]);
-    }
-    console.log(`For loop: ${forLoop}`);
+    ctx.strokeStyle = "#000000";
+    ctx.moveTo(m.round(w / scX / 2) * scX, 0);
+    ctx.lineTo(m.round(w / scX / 2) * scX, h);
+    ctx.moveTo(0, m.round(h / scY /2) * scY);
+    ctx.lineTo(w, m.round(h / scY /2) * scY);
     ctx.stroke();
+    ctx.closePath();
+    // Graph
+    for (let i = 0; i <= w; i++) {
+        ctx.fillRect(((i - m.round(w / scX / 2) * scX) / scX) * scX +
+        m.round(w / scX / 2) * scX, m.round(h / scY /2) * scY - scY *
+        m.log(m.pow(((i - m.round(w / scX / 2) * scX) / scX), 2) +
+        m.exp(m.abs(((i - m.round(w / scX / 2) * scX) / scX))) + 1), 4, 4);
+    }
 }
